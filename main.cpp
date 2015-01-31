1b80e6ab26a1f2ed64a02d0b695decb2695687d2
Parag Pardeshi <njoy7722@yahoo.com>
2015-01-31 13:17:02 -0800

    added namespace

--- main.cpp
+++ main.cpp
@@ -8,12 +8,15 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
     std::cout << "Hello, World \nChange 1!\n";
 	std::cout << "Change 2!\n";
+    
 
     return 0;
 }
